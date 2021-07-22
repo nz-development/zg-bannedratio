5f38baec77149f18af5a2d7948b79f601fab1ede
nz-development <85889358+nz-development@users.noreply.github.com>
2021-07-22 21:09:56 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,11 @@
+Config = Config or {}
+
+Config.BannedRatio = { --If you want to add a banned Radio just add here the ratio you want like: ["GetAspectRatio()"] = "Label of aspect ratio"
+    ["1.25"] = "5/4", -- 5:4
+    ["1.3333333730698"] = "4/3", -- 4:3
+    ["1.6666666269302"] = "5/3", -- 5:3
+    ["1.5"] = "3/2" -- 3:2
+}
+
+Config.TimeBeforeKick = (20*1000) --After 20 seconds the player will be kicked
+Config.KickMessage = "You got kicked for using a tryharder aspect ratio"
